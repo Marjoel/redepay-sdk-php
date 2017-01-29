faa3dc1d2ede4b86a648c5e9b0fde5c9f64cb2f7
Marjoel <marjoel@marjoel.com>
2017-01-29 19:15:32 -0200

    indentation refactoring

--- RedePay/Document/Document.php
+++ RedePay/Document/Document.php
@@ -1,53 +1,58 @@
 <?php
+/**
+*  @author   Marjoel Moreira [marjoel@marjoel.com]
+*  @license  https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
 namespace RedePay\Document;
 
 class Document {
-	use \RedePay\Utils\Fillable;
+    use \RedePay\Utils\Fillable;
 
-	/**
-	 * @param String
-	 */
-	private $kind;
+    /**
+     * @param String
+     */
+    private $kind;
 
-	/**
-	 * @param String
-	 */
-	private $number;
+    /**
+     * @param String
+     */
+    private $number;
 
-	/**
-	 * @param Document
-	 */
-	public function __construct($data = null) {
-		if(isset($data)) {
-			$this->fill($data);
-		}
-	}
+    /**
+     * @param Document
+     */
+    public function __construct($data = null) {
+        if(isset($data)) {
+            $this->fill($data);
+        }
+    }
 
-	/**
-	 * @return String
-	 */
-	public function getKind() {
-		return $this->kind;
-	}
+    /**
+     * @return String
+     */
+    public function getKind() {
+        return $this->kind;
+    }
 
-	/**
-	 * @return String
-	 */
-	public function getNumber() {
-		return $this->number;
-	}
+    /**
+     * @return String
+     */
+    public function getNumber() {
+        return $this->number;
+    }
 
-	/**
-	 * @param String
-	 */
-	public function setKind($kind) {
-		$this->kind = $kind;
-	}
+    /**
+     * @param String
+     */
+    public function setKind($kind) {
+        $this->kind = $kind;
+    }
 
-	/**
-	 * @param String
-	 */
-	public function setNumber($number) {
-		$this->number = $number;
-	}
+    /**
+     * @param String
+     */
+    public function setNumber($number) {
+        $this->number = $number;
+    }
 }
